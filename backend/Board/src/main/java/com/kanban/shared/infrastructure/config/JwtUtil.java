c5a6d39955cebc30e804a388ebb222878efc414b
Alexey <cfgI@yandex.ru>
2025-08-07 08:36:48 +0500

    Update JwtUtil.java

--- backend/Board/src/main/java/com/kanban/shared/infrastructure/config/JwtUtil.java
+++ backend/Board/src/main/java/com/kanban/shared/infrastructure/config/JwtUtil.java
@@ -22,10 +22,10 @@ public class JwtUtil {
 
     private String createToken(Map<String, Object> claims, String subject) {
         return Jwts.builder()
-                .setClaims(claims)
-                .setSubject(subject)
-                .setIssuedAt(new Date(System.currentTimeMillis()))
-                .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 60 * 10))
+                .claims(claims)
+                .subject(subject)
+                .issuedAt(new Date(System.currentTimeMillis()))
+                .expiration(new Date(System.currentTimeMillis() + 1000 * 60 * 60 * 10))
                 .signWith(SECRET_KEY)
                 .compact();
     }
